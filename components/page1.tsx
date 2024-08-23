02f3c1994f0030e6696c2a3087314841b7057267
Helixu38 <vipase2004@gmail.com>
2024-08-23 22:01:04 +0700

    implement page 1 2 3

---
+++ components/page1.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function Page1() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-[#5e75e8]">
+      <div className="bg-[#f6f1ed] p-20 sm:p-32 lg:p-64 my-10">
+        <div className="text-center">
+          <div className="text-7xl font-bold">
+            <span className="inline-block text-[#efb969] relative">
+              i<span className="absolute -top-2 -left-1"></span>
+            </span>
+            <span className="inline-block text-[#e07632]">t</span>
+            <span className="inline-block text-[#64bf90]">I</span>
+            <span className="inline-block text-[#fc5f57]">S</span>
+            <span className="inline-block text-[#e7a3d8]">N</span>
+            <span className="inline-block text-[#b598f8]">’t</span>
+          </div>
+          <div className="text-7xl font-bold mt-6">
+            <span className="inline-block text-[#83c0bb]">y</span>
+            <span className="inline-block text-[#fc5f57] relative">
+              O<span className="absolute inset-0"></span>
+            </span>
+            <span className="inline-block text-[#f7d179]">U</span>
+            <span className="inline-block text-[#4a68cb]">r</span>
+          </div>
+          <div className="text-7xl font-bold mt-6">
+            <span className="inline-block text-[#efb969]">F</span>
+            <span className="inline-block text-[#e7a3d8]">A</span>
+            <span className="inline-block text-[#4a68cb]">U</span>
+            <span className="inline-block text-[#83c0bb]">L</span>
+            <span className="inline-block text-[#fc5f57]">T</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/page2.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Image from "next/image";
+
+export default function page2() {
+  return (
+    <div className="relative bg-[url('/img/page2.png')] bg-contain bg-no-repeat h-screen bg-center">
+      <div className="absolute left-120 bottom-66 text-base text-white">
+      </div>
+    </div>
+  );
+}
